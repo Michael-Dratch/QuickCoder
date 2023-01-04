433da96128b952bcacc58b1f6bb959d2f48cec97
Michael Dratch <michael.s.dratch@gmail.com>
2023-01-04 13:16:15 -0500

    implemented project crud operations

--- database/database.py
+++ database/database.py
@@ -9,35 +9,70 @@ class Database:
         self.conn = None
         self.cursor = None
 
+    def initializeDatabase(self):
+        self.createConnection()
+        self.cursor.execute("""PRAGMA foreign_keys = ON""")
+        self.conn.commit()
+
     def createConnection(self, name):
         self.conn = sqlite3.connect(name)
         self.cursor = self.conn.cursor()
 
+    # Used for testing
     def setConnection(self, connection):
         self.conn = connection
         self.cursor = connection.cursor()
+        self.cursor.execute("""PRAGMA foreign_keys = ON""")
+        self.conn.commit()
 
     def closeConnection(self):
         self.conn.close()
 
-    def createTable(self, name, fields):
-        self.cursor.execute("""CREATE TABLE {} {}""".format(name, fields))
-        self.conn.commit()
-
     def initializeTables(self):
-        self.createTable('code', '(id integer PRIMARY KEY, name text, color text, project integer)')
-        self.createTable('document', '(id integer PRIMARY KEY, name text, html text)')
+        self.createProjectTable()
+        self.createDocumentTable()
+        self.createCodeTable()
+        self.createCodeInstanceTable()
+
+    def createProjectTable(self):
         self.createTable('project', '(id integer PRIMARY KEY, name text)')
+
+    def createDocumentTable(self):
+        self.createTable('document', '(id integer PRIMARY KEY, '
+                                     'name text, '
+                                     'html text, '
+                                     'project_id integer, '
+                                     'FOREIGN KEY (project_id) REFERENCES project (id))')
+
+    def createCodeTable(self):
+        self.createTable('code', '(id integer PRIMARY KEY, '
+                                 'name text, color text, '
+                                 'project integer,'
+                                 'FOREIGN KEY (project) REFERENCES project (id)) ')
+
+    def createCodeInstanceTable(self):
         self.createTable('codeinstance', '(id integer PRIMARY KEY, '
                                          'document integer, '
                                          'text text, '
                                          'start integer,'
                                          'end integer)')
 
-    def saveCode(self, name, color, projectId):
+    def createTable(self, name, fields):
+        sql = """CREATE TABLE {} {}""".format(name, fields)
+        self.cursor.execute(sql)
+        self.conn.commit()
+
+    def createCode(self, name, color, projectId):
+        if self.codeExists(name):
+            raise Exception('Code name already exists')
         sql = """INSERT INTO code (id, name, color, project) VALUES (NULL, :name, :color, :project)"""
         self.cursor.execute(sql, {'project': projectId, 'name': name, 'color': color})
         self.conn.commit()
+
+    def codeExists(self, name):
+        self.cursor.execute("""SELECT * FROM code WHERE name=:name""", {'name': name})
+        codes = self.cursor.fetchall()
+        return len(codes) > 0
 
     def deleteCode(self, codeId):
         sql = """DELETE FROM code WHERE id=:codeId"""
@@ -63,3 +98,33 @@ class Database:
             code = Code(row[0], row[1], row[2], row[3])
             codes.append(code)
         return codes
+
+    def createProject(self, name):
+        if self.project_exists(name):
+            raise Exception('Project name already exists')
+        sql = """INSERT INTO project (id, name) VALUES (NULL, :name)"""
+        self.cursor.execute(sql, {'name': name})
+        self.conn.commit()
+
+    def project_exists(self, name):
+        self.cursor.execute("""SELECT * FROM project WHERE name=:name""", {'name': name})
+        projects = self.cursor.fetchall()
+        return len(projects) > 0
+
+    def deleteProject(self, name):
+        if not self.project_exists(name):
+            return
+        sql = """DELETE FROM project WHERE name=:name"""
+        self.cursor.execute(sql, {'name': name})
+        self.conn.commit()
+
+    def updateProject(self, oldName, newName):
+        if not self.project_exists(oldName):
+            return
+        sql = """UPDATE project
+                    SET name = :newName
+                    WHERE name = :oldName"""
+        self.cursor.execute(sql, {'newName': newName,
+                                  'oldName': oldName})
+        self.conn.commit()
+
--- database/database_test.py
+++ database/database_test.py
@@ -30,10 +30,15 @@ class DatabaseTest(unittest.TestCase):
                                                     (tableName,)).fetchone()
         self.assertTrue(str(sql_table_description).__contains__(fieldName))
 
-    def codeExists(self, id, codeName, color, projectId):
-        self.cursor.execute("""SELECT * FROM code""")
-        data = self.cursor.fetchone()
-        return str(data) == """({}, '{}', '{}', {})""".format(id, codeName, color, projectId)
+    def codeExists(self, codeName):
+        self.cursor.execute("""SELECT * FROM code WHERE name=:name""", {'name': codeName})
+        data = self.cursor.fetchall()
+        return len(data) == 1
+
+    def projectExists(self, name):
+        self.cursor.execute("""SELECT * FROM project WHERE name=:name""", {'name': name})
+        data = self.cursor.fetchall()
+        return len(data) == 1
 
     def getNumRows(self, tableName):
         self.cursor.execute("""SELECT * FROM {}""".format(tableName))
@@ -46,12 +51,20 @@ class DatabaseTest(unittest.TestCase):
         self.assertTableExists("project")
         self.assertTableExists("codeinstance")
 
-    def test_save_code(self):
-        self.db.saveCode('positive', '#00FF00', 1)
-        self.assertTrue(self.codeExists(1, 'positive', '#00FF00', 1))
+    def test_create_code(self):
+        self.db.createProject('test_project')
+        self.db.createCode('positive', '#00FF00', 1)
+        self.assertTrue(self.codeExists('positive'))
+
+    def test_create_duplicate_code_fails(self):
+        self.db.createProject('project')
+        self.db.createCode('code', 'blue', 1)
+        with self.assertRaises(Exception):
+            self.db.createCode('code', 'green', 1)
 
     def test_delete_code(self):
-        self.db.saveCode('positive', '#00FF00', 1)
+        self.db.createProject('test_project')
+        self.db.createCode('positive', '#00FF00', 1)
         self.db.deleteCode(codeId=1)
         self.assertEqual(0, self.getNumRows('code'))
 
@@ -60,22 +73,67 @@ class DatabaseTest(unittest.TestCase):
         newName = 'newName'
         color = '#00FF00'
         projectId = 1
-        self.db.saveCode(oldName, color, projectId)
+        self.db.createProject('test_project')
+        self.db.createCode(oldName, color, projectId)
         self.db.updateCode(1, newName, color)
-        self.assertTrue(self.codeExists(1, newName, color, projectId))
+        self.assertTrue(self.codeExists(newName))
 
     def test_getProjectCodes_None_Exist(self):
         codes = self.db.getProjectCodes(1)
         self.assertEqual(len(codes), 0)
 
     def test_getProjectCodes_Two_Exist(self):
-        self.db.saveCode('code1', '#FFFFFF', 1)
-        self.db.saveCode('code2', '#FFFFFF', 1)
+        self.db.createProject('test_project')
+        self.db.createCode('code1', '#FFFFFF', 1)
+        self.db.createCode('code2', '#FFFFFF', 1)
         codes = self.db.getProjectCodes(1)
         self.assertEqual(len(codes), 2)
         self.assertEqual(codes[0].name, 'code1')
         self.assertEqual(codes[1].name, 'code2')
 
+    def test_foreign_key_constraints(self):
+        with self.assertRaises(sqlite3.IntegrityError):
+            self.db.createCode('name', 'blue', 2)
+
+    def test_create_project(self):
+        self.db.createProject('test_project')
+        self.assertTrue(self.projectExists('test_project'))
+
+    def test_creating_duplicate_project_fails(self):
+        self.db.createProject('project')
+        with self.assertRaises(Exception):
+            self.db.createProject('project')
+
+    def test_delete_non_existant_project(self):
+        self.db.deleteProject('project')
+
+    def test_delete_project(self):
+        self.db.createProject('project')
+        self.db.deleteProject('project')
+        self.assertFalse(self.projectExists('project'))
+
+    def test_update_non_existent_project_fails(self):
+        self.db.updateProject('oldName', 'newName')
+
+    def test_update_project_name(self):
+        self.db.createProject('oldName')
+        self.db.updateProject('oldName', 'newName')
+        self.assertFalse(self.projectExists('oldName'))
+        self.assertTrue(self.projectExists('newName'))
+
+
+    def test_create_document(self):
+        self.db.createProject('test_project')
+        self.db.createDocument('doc', 1)
+        self.assertTrue(self.documentExists('doc'))
+
+    # def test_create_duplicate_document_fails(self):
+    #     self.db.createProject('project')
+    #     self.db.createCode('code', 'blue', 1)
+    #     with self.assertRaises(Exception):
+    #         self.db.createCode('code', 'green', 1)
+
 
 if __name__ == '__main__':
     unittest.main()
+
